494ec1228c4917d80c9c1a440f8147f6bf1b5f7b
Abszissex <rubSXkwZDZGqrr4>
2021-11-24 20:46:04 +0100

    Step 1

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,13 +2,29 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
+
+# To be honest I am not sure if this was the idea behind it
+# but since we don't keep multiple open connections to the database
+# I don't think it would make sense otherwise.
+db_connection_count = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
+    global db_connection_count
+    db_connection_count += 1
     connection.row_factory = sqlite3.Row
     return connection
+
+# Function to get the number of posts in the database
+def get_post_count():
+    connection = get_db_connection()
+    post_count_row = connection.execute('SELECT COUNT(*) AS count FROM posts').fetchone()
+    connection.close()
+
+    return post_count_row['count']
 
 # Function to get a post using its ID
 def get_post(post_id):
@@ -16,6 +32,7 @@ def get_post(post_id):
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
     connection.close()
+
     return post
 
 # Define the Flask application
@@ -30,14 +47,42 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
+
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+@app.route('/metrics')
+def metrics():
+    global db_connection_count
+    total_posts_in_db = get_post_count()
+
+
+    response = app.response_class(
+            response=json.dumps({"db_connection_count": db_connection_count, "post_count": total_posts_in_db}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+
 # Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('Found no article for id: %d', post_id)
       return render_template('404.html'), 404
     else:
+      app.logger.info('Retrieved article: %s', post['title'])
       return render_template('post.html', post=post)
 
 # Define the About Us page
@@ -67,4 +112,5 @@ def create():
 
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(level=logging.DEBUG)
+    app.run(host='0.0.0.0', port='3111')
